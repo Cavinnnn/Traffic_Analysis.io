0b89290890f8030f335abae63c37a16968d7b255
Cavin O' sullivan <cavinosullivan@hotmail.com>
2019-04-23 20:33:39 +0100

    cleanup

--- server.js
+++ server.js
@@ -3,28 +3,9 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(__dirname + '/dist/web-app/'));
-app.use(allowCrossDomain);
-
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-}
-
-app.get('/back-end', function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-});
 
 app.get('*/', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/web-app/index.html'));
 });
-
-
-var cors = require('cors')
-const cors = require('cors');
-app.options('*', cors());
-
-app.use(cors());
 
 app.listen(process.env.PORT || 8080);
